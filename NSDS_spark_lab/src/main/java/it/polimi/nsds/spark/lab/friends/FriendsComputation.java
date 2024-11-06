4b838d96ea0cfb5ee5db69783ff25b2e0d9f7726
Pitesse <pietro.pizzoccheri@mail.polimi.it>
2024-11-06 14:51:13 +0100

    lab exercises

--- NSDS_spark_lab/src/main/java/it/polimi/nsds/spark/lab/friends/FriendsComputation.java
+++ NSDS_spark_lab/src/main/java/it/polimi/nsds/spark/lab/friends/FriendsComputation.java
@@ -10,18 +10,14 @@ import org.apache.spark.sql.types.StructType;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Implement an iterative algorithm that implements the transitive closure of friends
- * (people that are friends of friends of ... of my friends).
- *
- * Set the value of the flag useCache to see the effects of caching.
- */
+import static org.apache.spark.sql.functions.col;
+
 public class FriendsComputation {
     private static final boolean useCache = true;
 
     public static void main(String[] args) {
         final String master = args.length > 0 ? args[0] : "local[4]";
-        final String filePath = args.length > 1 ? args[1] : "./";
+        final String filePath = args.length > 1 ? args[1] : "./NSDS_spark_lab/";
         final String appName = useCache ? "FriendsCache" : "FriendsNoCache";
 
         final SparkSession spark = SparkSession
@@ -36,14 +32,38 @@ public class FriendsComputation {
         fields.add(DataTypes.createStructField("friend", DataTypes.StringType, false));
         final StructType schema = DataTypes.createStructType(fields);
 
-        final Dataset<Row> input = spark
+        Dataset<Row> friends = spark
                 .read()
                 .option("header", "false")
                 .option("delimiter", ",")
                 .schema(schema)
                 .csv(filePath + "files/friends/friends.csv");
 
-        // TODO
+        if (useCache) {
+            friends = friends.cache();
+        }
+
+        Dataset<Row> transitiveClosure = friends;
+        int distance = 0;
+        final int MAX_DISTANCE = 1000;
+
+        while (distance < MAX_DISTANCE) {
+            Dataset<Row> newFriends = transitiveClosure
+                    .as("tf")
+                    .join(friends.as("f"), col("tf.friend").equalTo(col("f.person")))
+                    .select(col("tf.person").alias("person"), col("f.friend").alias("friend"))
+                    .except(transitiveClosure);
+
+            if (newFriends.isEmpty()) {
+                break;
+            }
+
+            transitiveClosure = transitiveClosure.union(newFriends).distinct();
+            distance++;
+        }
+
+        transitiveClosure.orderBy("person", "friend");
+        transitiveClosure.show();
 
         spark.close();
     }
